831bf70f92157f92c26dc745a6237bf2b7b0f6db
Sai Deepika Gopala <gopalasaideepika@gmail.com>
2018-03-20 16:54:16 -0400

    Book List

--- MobileBookStore_SaiDeepika/ViewControllers/MBHomeViewController.swift
+++ MobileBookStore_SaiDeepika/ViewControllers/MBHomeViewController.swift
@@ -9,27 +9,70 @@
 import UIKit
 
 class MBHomeViewController: UIViewController {
-
+    //MARK: - Properties
+    
+    @IBOutlet weak var bookListTableView: UITableView!
+    var booksArray = [MBBookModel] ()
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = MBConstants.ScreenTitles.MainScreenTitle
+        
+        bookListTableView.register(UINib.init(nibName: MBConstants.NibNames.BookTableViewNib, bundle: nil), forCellReuseIdentifier: MBConstants.CellIdentifiers.BookTableViewCellIdentifier)
+        
+       //Fetching data using service call
+        MBServiceManager.shared.downloadDataFromURL(url: URL(string: MBConstants.ServiceUrls.MainUrl)!) { booksData in
+            self.booksArray = booksData
+            DispatchQueue.main.async {
+                //reload the table
+                self.bookListTableView.reloadData()
+                
+            }
     }
+}
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    extension MBHomeViewController:UITableViewDataSource,UITableViewDelegate{
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return self.booksArray.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: MBConstants.CellIdentifiers.BookTableViewCellIdentifier, for: indexPath) as! MBBookTableViewCell
+            let individualBookDetails = self.booksArray[indexPath.row]
+            
+            cell.bookTitle.text = individualBookDetails.title ?? ""
+            
+            if let authorName = individualBookDetails.author{
+                cell.bookAuthor.text = MBConstants.HelperTexts.ByText + authorName
+            }else{
+                cell.bookAuthor.text = ""
+            }
+            
+            
+            if let imageUrl = individualBookDetails.bookImageUrl{
+                MBServiceManager.shared.downloadImageFromURL(url: imageUrl) { downloadedImage in
+                    DispatchQueue.main.async {
+                        cell.bookImageView.image = downloadedImage
+                    }
+                }
+            }
+            
+            return cell
+        }
+        //Establishing push segue to the book detail view controller
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let bookModel = self.booksArray[indexPath.row]
+            let storyBoard : UIStoryboard = UIStoryboard(name: MBConstants.StoryboardIdentifiers.MainStoryboardIdentifier, bundle:nil)
+            let bookDetailsViewController = storyBoard.instantiateViewController(withIdentifier: MBConstants.StoryboardIdentifiers.MBDetailViewControllerIdentifier) as! MBDetailViewController
+            bookDetailsViewController.bookModel = bookModel
+            self.navigationController?.pushViewController(bookDetailsViewController, animated: true)
+            
+        }
+        
 }
