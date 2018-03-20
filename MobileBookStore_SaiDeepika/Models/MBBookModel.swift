530aaeba7d9a83487ae9685c8907c1f78e1d1865
Sai Deepika Gopala <gopalasaideepika@gmail.com>
2018-03-20 15:20:09 -0400

    Setting up Folder Structure for MVC

---
+++ MobileBookStore_SaiDeepika/Models/MBBookModel.swift
@@ -0,0 +1,9 @@
+//
+//  MBBookModel.swift
+//  MobileBookStore_SaiDeepika
+//
+//  Created by Minnie . on 3/20/18.
+//  Copyright © 2018 sample. All rights reserved.
+//
+
+import Foundation
---
+++ MobileBookStore_SaiDeepika/ServiceManager/MBServiceManager.swift
@@ -0,0 +1,9 @@
+//
+//  MBServiceManager.swift
+//  MobileBookStore_SaiDeepika
+//
+//  Created by Minnie . on 3/20/18.
+//  Copyright © 2018 sample. All rights reserved.
+//
+
+import Foundation
