3fc475038b05dbae8ceafbac8d5d7f242eb3421e
Sai Deepika Gopala <gopalasaideepika@gmail.com>
2018-03-20 16:09:31 -0400

    Creating model

--- MobileBookStore_SaiDeepika/Models/MBBookModel.swift
+++ MobileBookStore_SaiDeepika/Models/MBBookModel.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class MBBookModel{
+    private(set) public var title:String?
+    private(set) public var bookImageUrl:String?
+    private(set) public var author:String?
+    
+    init(bookDict:[String:String]) {
+        
+        if let title = bookDict["title"]{
+            self.title = title
+        }
+        
+        if let imageUrl = bookDict["imageURL"]{
+            self.bookImageUrl = imageUrl
+        }
+        
+        if let authorName = bookDict["author"]{
+            self.author = authorName
+        }
+    }
+}
